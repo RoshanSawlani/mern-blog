aad952a163244406a3de26553869fda94724afc1
RoshanSawlani <roshansawlani@gmail.com>
2024-03-20 19:29:08 +0530

    create signin api route

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -14,7 +14,7 @@ export default function FooterCom() {
                         >
                             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
                                 Roshan's
-                            </span>{" "}
+                            </span>
                             Blog
                         </Link>
                     </div>
