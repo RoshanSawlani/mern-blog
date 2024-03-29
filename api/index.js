423ecc8e874409e3737c68222f2fd6dc5f7a0e31
RoshanSawlani <roshansawlani@gmail.com>
2024-03-29 14:55:11 +0530

    add create a post api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
+import postRoutes from './routes/post.route.js'
 import cookieParser from 'cookie-parser';
 
 dotenv.config()
@@ -23,13 +24,14 @@ app.listen(3004, () => {
 
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
+app.use('/api/post', postRoutes)
 
 
-app.use((err,req,res,next)=>{
+app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
     res.status(statusCode).json({
-        success:false,
+        success: false,
         statusCode,
         message
     })
