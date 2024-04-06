d60365437cf04a1b1afd072719a22a75bc8a4fe7
RoshanSawlani <roshansawlani@gmail.com>
2024-04-06 19:35:03 +0530

    add edit functionality to the comment component

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -3,6 +3,7 @@ import { Link,useNavigate } from 'react-router-dom';
 import { Alert, Button, Textarea } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import Comment from './Comment';
+import { editComment } from '../../../api/controllers/comment.controller';
 
 
 export default function CommentSection({postId}) {
@@ -74,6 +75,10 @@ export default function CommentSection({postId}) {
             console.log(error.message)
         }
     }
+    const handleEdit = async(comment,editedContent) =>{
+        setComments(comments.map((c)=>c._id === comment._id ? {...c,content:editedContent}:c))
+    }
+
 return (
     <div className='max-w-2xl mx-auto w-full p-3'>
         {currentUser ? (
@@ -117,7 +122,7 @@ return (
             </div>
             {
             comments.map((comment)=>(
-                <Comment key={comment._id} comment={comment} onLike={handleLike}/>
+                <Comment key={comment._id} comment={comment} onLike={handleLike} onEdit={handleEdit}/>
             ))
             }
             </>
